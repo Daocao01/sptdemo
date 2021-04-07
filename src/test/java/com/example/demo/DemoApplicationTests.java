348b6caee28db3478e712495c4c9916ecc8fd451
Daocao01 <zhangli137850>
2021-04-07 17:50:56 +0800

    222

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public static void main(String[] args){
+		System.out.println("333");
+	}
+}
